35b6c9ad730e5090339798c4d9fc1a9a05012db5
guanaberto <danielguana@gmail.com>
2021-06-17 12:06:44 -0400

    First Angular Version

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -2,15 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FrameworkComponent } from './framework/framework.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    FrameworkComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomePageComponent
+      }
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
