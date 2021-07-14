752fcf60d470b9b3911e52c172a21bb018bcd090
gurminderSingh1995 <75852890+gurminderSingh1995@users.noreply.github.com>
2021-07-14 12:02:29 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+// my version
 //Adds the reference to DB
 require('./app_server/models/db');
 
