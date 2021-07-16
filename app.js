421e62a7adf358b95a1fcdbbd8c1441f815f27c8
guanaberto <danielguana@gmail.com>
2021-07-16 18:05:56 -0400

    Version 3

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // to add in the future and relate with Angular
 app.use('/api',(req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  //res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Origin', 'https://bakingbella.herokuapp.com');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
   next();
