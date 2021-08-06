b89836fa667b0720d8362c27bdc4564f8f78999e
guanaberto <danielguana@gmail.com>
2021-08-06 15:15:53 -0400

    Sprint 3

--- app_public/src/app/bakingbellavars.ts
+++ app_public/src/app/bakingbellavars.ts
@@ -1,6 +1,6 @@
 
 export class BakingBellaVars{
-    public static defaultEmail = "bakingbella7@gmail.com";
+    public static defaultEmail = "bakingbellabakery@gmail.com";
     public static defaultContactSubject = "Contact Form Message";
     public static defaultApprovedEventSubject = "Your Event has been Approved";
     public static defaultRejectedEventSubject = "Your Event has been Rejected";
